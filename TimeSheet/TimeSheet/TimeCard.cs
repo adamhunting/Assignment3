6371f06a790620a984b9a88121694749c70679f9
adam.hunting <adam.hunting@gmail.com>
2016-07-06 13:57:00 -0600

    WIP - TimeCardTest added

--- TimeSheet/TimeSheet/TimeCard.cs
+++ TimeSheet/TimeSheet/TimeCard.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeSheet;
 
 namespace TimeSheet
 {
     class TimeCard
     {
+        private readonly Day[] _days;
+
+        public TimeCard( )
+        {
+            _days = new Day[14];
+        }
+        public Day[] GetDays()
+        {
+            return _days.ToArray();
+        }
     }
 }
---
+++ TimeSheet/TimeSheetTests/TimeCardTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeSheet;
+
+namespace TimeSheetTests
+{
+    [TestClass]
+    public class TimeCardTests
+    {
+        private Day _classUnderTest;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var date = new DateTime(2016, 7, 4);
+            _classUnderTest = new Day(date);
+            
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Init_Creates_Two_Week_Of_Consecutive_Days()
+        {
+            //Arrange
+           // var expeceted = 14;
+
+            //ACT
+           // var actual =
+
+
+            //Assert
+        }
+
+
+    }
+}
